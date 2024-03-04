a46ede6f17097063d81c58165f123b4da19b9aae
Axel Cabral <axel.cabral0102@gmail.com>
2024-03-03 23:21:18 -0300

    Updt 1.6
    - Criação do github actions run command para unit tests
    - Criados os arquivos controllers (ainda vazios)
    - Criados arquivos de configuração ou pré desenvolvimento de testes E2E
    - Configurado token jwt no app (ainda não testado)

---
+++ src/utils/test/create-and-authenticate-org.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateOrg(app: FastifyInstance) {
+  await prisma.org.create({
+    data: {
+      name: 'TypeScript Organization',
+      owner_name: 'Axel Cabral',
+      email: 'axel@gmail.com',
+      whatsapp: '+55 55 991332372',
+      password: await hash('123456', 6),
+      cep: '97574690',
+      state: 'RS',
+      city: 'Santana do Livramento',
+      neighborhood: 'Centro',
+      street: 'Rua teste, 80',
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    },
+  })
+
+  const authResponse = await request(app.server).post('/sessions').send({
+    email: 'axel@gmail.com',
+    password: '123456',
+  })
+
+  const { token } = authResponse.body
+
+  return {
+    token,
+  }
+}
